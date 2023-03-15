b4bb46ae21b7ba9a88c41752aee318e1f174a099
Harshit Sandilya <2021ucp1382@mnit.ac.in>
2023-03-15 20:23:52 +0530

    changed db

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 require("dotenv").config();
 
 mongoose.set("strictQuery", true);
-const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.p5xwxcl.mongodb.net/?retryWrites=true&w=majority`;
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.zsunw1o.mongodb.net/?retryWrites=true&w=majority`;
 mongoose
 	.connect(uri, {
 		waitQueueTimeoutMS: 5000,
